abcb5d9e57a3535a0fbc0e33304c8f8789636a84
Alexander Mariona <agmariona@college.harvard.edu>
2020-04-16 14:27:16 -0400

    Bookkeeping

--- knn_mst_hist/logparse.py
+++ knn_mst_hist/logparse.py
@@ -2,14 +2,16 @@ import numpy as np
 
 mst_entropies = []
 
-with open('log_cifar_l2_2.txt', 'r') as f:
+with open('logs/log_cifar_l0.txt', 'r') as f:
     color_i = -1
     for line in f:
         words = line.split()
         if words and words[0] == 'COLOR':
             mst_entropies.append([])
             color_i += 1
-        if words and words[0] == 'MST':
+        if words and words[0] == 'Random':
             mst_entropies[color_i].append(float(words[2]))
+            # mst_entropies.append(float(words[2]))
 
 print(np.average(mst_entropies, axis=1))
+# print(np.average(mst_entropies))
